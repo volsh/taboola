e16b750b6c95d32b7472182a4a18834666b5a463
lior volshtein <liorvolsh@gmail.com>
2022-02-14 17:23:16 +0200

    solution completed

--- src/components/TodoList/TodoItem/TodoItem.jsx
+++ src/components/TodoList/TodoItem/TodoItem.jsx
@@ -3,22 +3,23 @@ import styles from "./styles.module.scss";
 const TodoItem = ({
   userId,
   id,
-  index,
+  index, // I used the array position and not id for performance (o(1) instead of searching the todo by id when updating/deleting)
   title,
   completed,
   toggleComplete,
   deleteTodo,
 }) => {
+  const handleClick = (event) => {
+    if (event.ctrlKey) {
+      deleteTodo(index);
+    } else {
+      toggleComplete(index);
+    }
+  }
   return (
     <div
       className={`${styles.container} ${completed ? styles.completed : ""}`}
-      onClick={(event) => {
-        if (event.ctrlKey) {
-          deleteTodo(index);
-        } else {
-          toggleComplete(index);
-        }
-      }}
+      onClick={handleClick}
       role="button"
       aria-checked={completed}
       aria-label={title}
